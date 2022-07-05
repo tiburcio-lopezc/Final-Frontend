93fac5ecc4a11530203bdb185c6b16e1ba72bda7
tiburcio <tiburciolopez14@gmail.com>
2022-07-04 23:36:47 -0300

    correccion de error para suma de total y subtotales

--- my-app/src/component/ProductosCargados.jsx
+++ my-app/src/component/ProductosCargados.jsx
@@ -10,9 +10,12 @@ export default function ProductosCargados ({datos,eliminarProducto, ProductoAEdi
   useEffect(()=>{isSelect===""? setDescativaBoton(false):setDescativaBoton(true)},[isSelect])
   
   let total=0;
-  datos.forEach(element => {total+=element.precioTotal});
   
+  datos.forEach(element => {total+=Number(element.precioTotal)});
   const rowEdit=(producto)=>{setIsSelect(producto.id);ProductoAEditar(producto); setDescativaBoton(true)}
+  
+  
+  
   
   return(
 <div className="container is-max-desktop">
@@ -21,13 +24,13 @@ export default function ProductosCargados ({datos,eliminarProducto, ProductoAEdi
       <thead>
        
         <tr>
-        <th ><abbr className="subtitle is-5" style={{textDecoration:"none"}} title="Fecha de carga"><strong>Fecha</strong></abbr></th>
-          <th><abbr className="subtitle is-5" style={{textDecoration:"none"}} title="es el rubro"><strong>Rubro</strong></abbr></th> 
-           <th><abbr className="subtitle is-5" style={{textDecoration:"none"}} title="Es el producto"><strong>Producto</strong></abbr></th>
+        <th ><abbr className="subtitle is-6" style={{textDecoration:"none"}} title="Fecha de carga"><strong>Fecha</strong></abbr></th>
+          <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title="es el rubro"><strong>Rubro</strong></abbr></th> 
+           <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title="Es el producto"><strong>Producto</strong></abbr></th>
         
-          <th><abbr className="subtitle is-5" style={{textDecoration:"none"}} title="Cantidad seleccionada"><strong>Cantidad</strong></abbr></th>
-          <th><abbr className="subtitle is-5" style={{textDecoration:"none"}} title="precio unitario"><strong>Precio Unitario</strong></abbr></th>
-          <th><abbr className="subtitle is-5" style={{textDecoration:"none"}} title="sub total"><strong>Total</strong></abbr></th>
+          <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title="Cantidad seleccionada"><strong>Cantidad</strong></abbr></th>
+          <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title="precio unitario"><strong>Precio Unidad</strong></abbr></th>
+          <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title="sub total"><strong>Subtotal</strong></abbr></th>
         </tr>
     
       </thead> 
@@ -37,15 +40,16 @@ export default function ProductosCargados ({datos,eliminarProducto, ProductoAEdi
          datos.length>0 ?
               datos.map((producto)=> 
                                 <tr  className={producto.id===isSelect? "is-selected": ""} key={producto.id}  >
-                                  <td className="subtitle is-5" style={{textDecoration:"none"}}>{producto.fecha} </td>
-                                  <td className="subtitle is-5" style={{textDecoration:"none"}}>{producto.rubro}</td>
-                                  <td className="subtitle is-5" style={{textDecoration:"none"}}>{producto.nombreProducto }</td>
-                                  <td className="subtitle is-5" style={{textDecoration:"none"}}>{producto.cantidad}</td>
-                                  <td className="subtitle is-5" style={{textDecoration:"none"}}>{producto.precioUnitario}</td>
-                                  <td className="subtitle is-5" style={{textDecoration:"none"}}>Total</td>
+                                  <td className="subtitle is-6" style={{textDecoration:"none"}}>{producto.fecha} </td>
+                                  <td className="subtitle is-6" style={{textDecoration:"none"}}>{producto.rubro}</td>
+                                  <td className="subtitle is-6" style={{textDecoration:"none"}}>{producto.nombreProducto }</td>
+                                  <td className="subtitle is-6" style={{textDecoration:"none"}}>{producto.cantidad}</td>
+                                  <td className="subtitle is-6" style={{textDecoration:"none"}}>{producto.precioUnitario}</td>
+                                  <td className="subtitle is-6" style={{textDecoration:"none"}}>{producto.precioTotal}</td>
+
                                   <div>
-                                    <td><button className="button is-success is-normal is-responsive" id={producto.id}  onClick={()=>rowEdit(producto)} disabled={desactivaBoton?true:false} >editar</button></td>
-                                    <td><button className="button is-danger is-normal is-responsive" id={producto.id}  onClick={(e)=>{eliminarProducto(e.target.id)}}disabled={desactivaBoton?true:false}>eliminar</button></td>
+                                    <td><button className="button is-success is-normal is-responsive" id={producto.id}  onClick={()=>rowEdit(producto)} disabled={desactivaBoton?true:false} >Editar</button></td>
+                                    <td><button className="button is-danger is-normal is-responsive" id={producto.id}  onClick={(e)=>{eliminarProducto(e.target.id)}}disabled={desactivaBoton?true:false}>Eliminar</button></td>
                                  </div>
                                  
                                </tr>
@@ -78,8 +82,8 @@ export default function ProductosCargados ({datos,eliminarProducto, ProductoAEdi
           <th></th>
           <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title=""></abbr></th>
           <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title=""></abbr></th>
-          <th><abbr className="subtitle is-6 has-text-weight-bold" style={{textDecoration:"none"}} title="">Total: $</abbr></th>
-          <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title=""> </abbr></th>
+          <th><abbr className="subtitle is-6 has-text-weight-bold" style={{textDecoration:"none"}}>Total: $ {total}</abbr></th>
+          <th><abbr className="subtitle is-6" style={{textDecoration:"none"}} title> </abbr></th>
         </tr>
       </tfoot>
      
